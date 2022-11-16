648864353428b20c18b24c7b78528f9210b3bed7
parker-martel <parkermartel48@gmail.com>
2022-11-16 01:20:15 -0800

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.hbs"],
+  theme: {
+    extend: {
+      colors: {
+        loginBlue: '#005BB1',
+        signupBlue: '#BEE0FF'
+
+      }
+    },
+  },
+  plugins: [],
+}
